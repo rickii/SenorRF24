57c128f85419d70801565e89d0d81960977715c7
Greg Long <long_greg_c@hotmail.com>
2015-09-26 21:01:53 +0200

    added the code for the arduino node

--- rf24_httpserver/index.js
+++ rf24_httpserver/index.js
@@ -4,6 +4,10 @@ var app = express();
 // For creating the outbound POST request
 var request = require('request');
 
+// Parses the post form data
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // For converting an object to a string
 var querystring = require('querystring');
 
@@ -11,27 +15,25 @@ var querystring = require('querystring');
 var location = "Gregs Dutch Sensor Network";
 
 // Each thingspeak channel has a seperate api key, this will map between our sensor nodes and the keys.
-var thingSpeakChannels = [{_id:2,apiKey:'4BLDCMJ2KGMKCRGN'},{_id:4,apiKey:'11111'}];
+var thingSpeakChannels = [{_id:4,apiKey:'4BLDCMJ2KGMKCRGN'},{_id:5,apiKey:'11111'}];
 
 // Just some random Latitudes and Longitudes where we pretend our sensors are.
 var coords = [{lat:52.069629,long:4.275921},{lat:52.075919,long:4.278144},{lat:52.075694,long:4.288126}];
 
 var thingSpeakUri = 'http://api.thingspeak.com/update';
 
-// Searches and array of objects for an _id
-var findById = function(collection, _id){
-    var len = collection.length;
+var getApiKey = function(_id){
+    var len = thingSpeakChannels.length;
 
     for(var i = 0; i<len; i++){
-        var item = collection[i];
-
-        if(item._id === _id) return item;
+        var item = thingSpeakChannels[i];
+        if(item._id == _id) return item.apiKey;
     }
 
     return false;
 };
 
-var server = app.listen(3000, function () {
+var server = app.listen(3000,  '10.10.2.2',  function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log('Example app listening at http://%s:%s', host, port);
@@ -39,6 +41,7 @@ var server = app.listen(3000, function () {
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+    console.log("Someone hit ur home page");
 });
 
 var getRandomCoords = function(){
@@ -51,7 +54,7 @@ var getRandomCoords = function(){
 app.post('/api/sensor', function (req, res) {
     console.log("Got a post from " + req.ip);
     if(!req.body.hasOwnProperty('temp') ||
-    !req.body.hasOwnProperty('nodeId'))  {
+    !req.body.hasOwnProperty('nodeid'))  {
         res.statusCode = 400;
         console.log("POST was bad");
         return res.send('Error 400: Post syntax incorrect.');
@@ -59,11 +62,11 @@ app.post('/api/sensor', function (req, res) {
     }
 
     var sensorData = {
-        nodeId: req.body.nodeId,
+        nodeId: req.body.nodeid,
         nodeIpAddress: req.ip,
         temp: req.body.temp
     }
-console.log("Got this sensor data from the post" + sensorData);
+console.log("Got this sensor data from the post" + sensorData.toString());
     res.send('Got a POST request');
 
     sendToCloud(sensorData);
@@ -72,7 +75,7 @@ console.log("Got this sensor data from the post" + sensorData);
 // A function to send sensor data to the thing speak cloud service
 var sendToCloud = function(sensorData){
     console.log("Getting sensorData ready for a post");
-    if (!object.hasOwnProperty('nodeId')){
+    if (!sensorData.hasOwnProperty('nodeId')){
         return;
     }
 
@@ -82,10 +85,10 @@ var sendToCloud = function(sensorData){
     sensorData.lat = position.lat;
     sensorData.long = position.long;
 
-    console.log("sensor data now looks like" + sensorData);
+    console.log("sensor data now looks like" + sensorData.toString());
 
     // Get the thing speak api key for this node
-    var apiKey = findById(thingSpeakChannels,sensorData.nodeId);
+    var apiKey = getApiKey(sensorData.nodeId);
 
     // Need to replace the key names to thingspeak fields
     sensorData.field1 = sensorData.nodeId;
@@ -104,7 +107,7 @@ var sendToCloud = function(sensorData){
     var formData = querystring.stringify(sensorData);
     var contentLength = formData.length;
     console.log("sensor data is ready to be posted" + formData);
-
+    require('request-debug')(request);
     // send a POST to thingspeak
     request({
         headers: {
@@ -116,12 +119,13 @@ var sendToCloud = function(sensorData){
         body: formData,
         method: 'POST'
     }, function (err, res, body) {
-        if (!error && response.statusCode == 200) {
+        if (!err && res.statusCode == 200) {
             console.log(body)
         }
         else {
             console.log("Thingspeak Error: " + err);
             console.log("Thingspeak response code: " + res.statusCode);
+            console.log("the body " + body)
         }
     });
 };
