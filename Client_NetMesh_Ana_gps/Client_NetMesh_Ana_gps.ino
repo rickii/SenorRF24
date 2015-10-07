df0d373404e2c02950b30a12119cc1890c969347
rickii <rpudo@une.edu.au>
2015-10-08 10:39:52 +1100

    modified contents

--- Client_NetMesh_Ana_gps/Client_NetMesh_Ana_gps.ino
+++ Client_NetMesh_Ana_gps/Client_NetMesh_Ana_gps.ino
@@ -1,5 +1,16 @@
 /*
+ /*
+   This sample sketch demonstrates the normal use of a TinyGPS++ (TinyGPSPlus) object.
+   It requires the use of SoftwareSerial, and assumes that you have a
+   9600-baud serial GPS device hooked up on pins 4(rx) and 3(tx).
+*/
+
  * *************************************************************************
+ * This particular sketch does not work in it's current format.
+ * The UNO and small micros are compiling to approx > 90% of capacity with the GPS modules included
+ * It is posted here for later improvements 
+ *
+ *
  * RF24Ethernet Arduino library by TMRh20 - 2014-2015
  *
  * Automated (mesh) wireless networking and TCP/IP communication stack for RF24 radio modules
